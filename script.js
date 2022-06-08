7a8885d568bf22072041621b29066b3f64eb8933
k_95.21 <100065508+kohta9521@users.noreply.github.com>
2022-06-08 18:57:52 +0900

    結合用　ファイル
    複合バージョン

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-let nav = document.querySelector("#navArea");
-let btn = document.querySelector(".toggle-btn");
-let mask = document.querySelector("#mask");
-const hero = document.querySelector(".hero");
-const slider = document.querySelector(".slider");
-const logo = document.querySelector("#logo");
-const menu = document.querySelector(".menu");
-const headline = document.querySelector(".headline");
+$(window).on('load',function(){ //画面遷移時にギャラリーの画像が被らないように、すべての読み込みが終わった後に実行する
 
-btn.onclick = () => {
-  nav.classList.toggle("open");
-};
-
-mask.onclick = () => {
-  nav.classList.toggle("open");
-};
-const tl = gsap.timeline();
-
-tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
-  .fromTo(
-    hero,
-    1.2,
-    { width: "100%" },
-    { width: "80%", ease: Power2.easeInOut }
-  )
-  .fromTo(
-    slider,
-    1.2,
-    { y: "-100%" },
-    { y: "0%", ease: Power2.easeInOut },
-    "-=1.2"
-  )
-  .fromTo(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
-  .fromTo(menu, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
-  .fromTo(headline, 0.5, { opacity: 0, x: 0 }, { opacity: 1, x: 30 }, "-=0.5");
+    //＝＝＝Muuriギャラリープラグイン設定
+    var grid = new Muuri('.grid', {
+    
+    //アイテムの表示速度※オプション。入れなくても動作します
+    showDuration: 600,
+    showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+    hideDuration: 600,
+    hideEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+      
+    // アイテムの表示/非表示状態のスタイル※オプション。入れなくても動作します
+      visibleStyles: {
+        opacity: '1',
+        transform: 'scale(1)'
+      },
+      hiddenStyles: {
+        opacity: '0',
+        transform: 'scale(0.5)'
+      }    
+    });
+    
+    //＝＝＝並び替えボタン設定
+    $('.sort-btn li').on('click',function(){      //並び替えボタンをクリックしたら
+      $(".sort-btn .active").removeClass("active"); //並び替えボタンに付与されているactiveクラスを全て取り除き
+      var className = $(this).attr("class");      //クラス名を取得
+      className = className.split(' ');       //「sortXX active」のクラス名を分割して配列にする
+      $("."+className[0]).addClass("active");     //並び替えボタンに付与されているクラス名とギャラリー内のリストのクラス名が同じボタンにactiveクラスを付与
+      if(className[0] == "sort00"){           //クラス名がsort00（全て）のボタンの場合は、
+         grid.show('');               //全ての要素を出す
+      }else{                      //それ以外の場合は
+        grid.filter("."+className[0]);        //フィルターを実行
+      }
+    });
+    
+    //＝＝＝ Fancyboxの設定
+    $('[data-fancybox]').fancybox({
+     thumbs: {
+        autoStart: true, //グループのサムネイル一覧をデフォルトで出す。不必要であればfalseに
+      },  
+    });
+      
+    });
